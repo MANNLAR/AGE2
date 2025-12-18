0d2f603d782a3c712282513799267691f76762cf
MANNLAR <m1aguilar@outlook.com>
2025-12-18 11:56:05 -0600

    Revert "Add Age Nexus static web project"
    This reverts commit 0b41e50cd12a4b338e3c1e504c97147c56c47804.

--- age-nexus/src/js/data/units.js
+++
@@ -1,7 +0,0 @@
-const UNITS = [
-  { name: 'Escaramuzador', class: 'arqueria', counter: 'Arqueros', tech: 'Agujas', cost: '25 madera', strength: 'Anti-arqueros', civ: 'Todas' },
-  { name: 'Lancero', class: 'infanteria', counter: 'Caballería', tech: 'Piques', cost: '35 comida, 25 madera', strength: 'Anti-caballos', civ: 'Todas' },
-  { name: 'Águila Guerrera', class: 'infanteria', counter: 'Arqueros/Monjes', tech: 'Saqueo', cost: '20 comida, 50 oro', strength: 'Movilidad', civ: 'Meso' },
-  { name: 'Arbalestero', class: 'arqueria', counter: 'Infantería ligera', tech: 'Química', cost: '35 madera, 40 oro', strength: 'DPS a distancia', civ: 'Todas' },
-  { name: 'Mangonel', class: 'asedio', counter: 'Grupos', tech: 'Cantería', cost: '160 madera, 135 oro', strength: 'Daño de área', civ: 'Todas' }
-];
